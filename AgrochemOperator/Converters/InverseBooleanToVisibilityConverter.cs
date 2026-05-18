544667fc6a42938ea8382445140a1860ff700380
KazaH <kprivet000@gmail.com>
2026-05-18 13:06:28 +0300

    Add files via upload

---
+++ AgrochemOperator/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AgrochemOperator.Converters
+{
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && boolValue)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AgrochemOperator/Converters/StatusToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AgrochemOperator.Converters
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string status = value as string ?? "";
+            switch (status)
+            {
+                case "completed":
+                    return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#22C55E"));
+                case "in_progress":
+                    return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0EA5E9"));
+                case "pending":
+                    return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F59E0B"));
+                case "skipped":
+                    return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#94A3B8"));
+                default:
+                    return new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
