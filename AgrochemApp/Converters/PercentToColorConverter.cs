04658445adfad02b8ac2eaeafb92fdd31851ac24
KazaH <kprivet000@gmail.com>
2026-05-18 12:57:45 +0300

    Add files via upload

---
+++ AgrochemApp/Converters/PercentToColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AgrochemApp.Converters
+{
+    public class PercentToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal percent = 0;
+
+            if (value is decimal dec)
+                percent = dec;
+            else if (value is int intVal)
+                percent = intVal;
+            else if (value is double dbl)
+                percent = (decimal)dbl;
+            else if (value is float fl)
+                percent = (decimal)fl;
+
+            if (percent == 100)
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#22C55E"));
+            }
+            else
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EF4444"));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AgrochemApp/Converters/StatusToColorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AgrochemApp.Converters
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string status = value as string ?? "";
+
+            if (status == "active" || status == "approved")
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#22C55E"));
+            }
+            else if (status == "draft")
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F59E0B"));
+            }
+            else if (status == "archived")
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6B7280"));
+            }
+            else if (status == "rejected")
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EF4444"));
+            }
+            else
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3B82F6"));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
